e97649d525dacff4cac3dbc6b456c1c0512d3b00
Rokas Vilkaitis <rokas.vilkaitis@mif.stud.vu.lt>
2020-03-07 19:09:39 +0200

    supaprastintas app template ir alpha kursu kurimo puslapis

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/create-course', 'CreateCourseController@index');
+
 Route::get('/kursai','RouteToCourse@index')->name('RouteToCourse');
 
 Route::group(['middleware' => 'auth'], function () {
