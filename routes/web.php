53592cbcb493494dbd4895da18bddce7ad8faf9c
Danys Valdemaras <valdemaras.danys@mif.stud.vu.lt>
2020-04-16 06:47:51 +0300

    Small changes

--- routes/web.php
+++ routes/web.php
@@ -18,22 +18,28 @@ Route::get('/', function () {
 
 Auth::routes();
 
+// Announcements/Homepage
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/home/action', 'HomeController@action')->name('home.action');
+
+Route::group(['middleware' => 'auth'], function(){
 Route::post('/home', 'HomeController@store')->name('home.store');
 Route::get('/announcements/{announcement_id}/edit', 'HomeController@edit')->name('announcement.edit');
 Route::patch('/announcements/{announcement_id}', 'HomeController@update')->name('announcement.update');
 Route::delete('/announcements/{announcement_id}', 'HomeController@destroy')->name('home.destroy');
+});
 
+// About page
 Route::get('/about', 'AboutController@index')->name('about');
 
+// D.U.K/F.A.Q page
 Route::get('/faq', 'FAQController@index')->name('faq');
 Route::post('/faq', 'FAQController@storeQuestion')->name('faq.store.question');
+Route::group(['middleware' => 'auth'], function(){
 Route::patch('/faq', 'FAQController@storeAnswer')->name('faq.store.answer');
 Route::delete('/faq/{faq_id}', 'FAQController@destroyById')->name('faq.destroy');
 Route::delete('/faq/{question}', 'FAQController@destroyByQ')->name('q.destroy');
-
-Route::get('/announcements/edit/{announcement_id}', 'EditAnnouncement@index')->name('editannouncement');
+});
 
 Route::get('/kursai','CourseController@index')->name('Kursai');
 
@@ -71,8 +77,11 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
 });
 
+// Data insertation page
+Route::group(['middleware' => ['auth' => 'admin']], function(){
 Route::get('/insertion', 'InsertionController@index')->name('iterpimas');
+Route::post('/insertion/fetch', 'InsertionController@fetch')->name('iterpimas.fetch');
 Route::post('/insertion/city', 'InsertionController@insertCity')->name('iterpimas.city');
 Route::post('/insertion/steam-center', 'InsertionController@insertSteamCenter')->name('iterpimas.steamCenter');
 Route::post('/insertion/room', 'InsertionController@insertRoom')->name('iterpimas.room');
-Route::post('/insertion/fetch', 'InsertionController@fetch')->name('iterpimas.fetch');
+});
